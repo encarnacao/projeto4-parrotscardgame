b532f9f30036bf751ad99d068e737c09a4b03a4b
encarnacao <cencarnacaoq@gmail.com>
2022-10-19 21:26:25 -0300

    Adicionado efeito 3D de carta virando

--- script.js
+++ script.js
@@ -16,9 +16,8 @@ function criarCarta(){
     const front = document.createElement('span');
     const back = document.createElement('span');
     carta.classList.add('card');
-    back.classList.add('hide');
-    back.setAttribute('id', 'back');
-    front.setAttribute('id', 'front');
+    back.setAttribute('class', 'back-face face');
+    front.setAttribute('class', 'front-face face');
     front.innerHTML = `<img src="media/front.png" />`;
     back.innerHTML = `<img src="media/metalparrot.gif" />`;
     carta.appendChild(front);
@@ -32,14 +31,17 @@ function virarCarta(){
      * @returns {void}
      */
     const carta = this;
-    carta.children[0].classList.toggle('hide');
-    carta.children[1].classList.toggle('hide');
+    carta.children[0].classList.toggle('front-face-rotate');
+    carta.children[1].classList.toggle('back-face-rotate');
 }
 
 
 
-//const iter = Number(prompt("Digite o número de cartas que deseja adicionar"));
-adicionarCarta(4);
+let numeroDeCartas;
+while((numeroDeCartas % 2 !== 0 || numeroDeCartas < 4) || numeroDeCartas > 14 ){
+    numeroDeCartas = Number(prompt('Digite um número par entre 4 e 14'));
+}
+ adicionarCarta(numeroDeCartas);
 
 const cartas = container.children;
 for(let i = 0; i < cartas.length; i++){
